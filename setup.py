b8b374776e6839665256fe3b140f7e81d87bd955
Zack Hodari <zackhodari@gmail.com>
2019-05-30 13:14:39 +0100

    Added basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+  
+setup(
+    name='morgana',
+    version='0.0.1',
+    description='Toolkit for defining and training Text-to-Speech voices in PyTorch.',
+    url='https://github.com/ZackHodari/morgana',
+    author='Zack Hodari',
+    author_email='zack.hodari@ed.ac.uk',
+    license='MIT',
+    packages=['morgana'])
+
