a4ae299581911a4b928b6e56bc913e69848390ce
Zack Hodari <zackhodari@gmail.com>
2019-10-31 11:02:46 +0000

    Updated inline with Morgana changes.

--- models/f0_test_model.py
+++ models/f0_test_model.py
@@ -8,9 +8,11 @@ import torch.nn as nn
 
 from morgana.base_models import BaseSPSS
 from morgana.experiment_builder import ExperimentBuilder
-from morgana import viz
+from morgana import data
+from morgana import losses
 from morgana import metrics
 from morgana import utils
+from morgana import viz
 
 from tts_data_tools import data_sources
 from tts_data_tools import file_io
@@ -23,7 +25,7 @@ class F0Model(BaseSPSS):
         self.input_dim = input_dim
         self.output_dim = output_dim
 
-        self.recurrent_layers = utils.SequentialWithRecurrent(
+        self.layers = utils.SequentialWithRecurrent(
             nn.Linear(self.input_dim, 256),
             nn.Sigmoid(),
             nn.Dropout(p=dropout_prob),
@@ -45,14 +47,22 @@ class F0Model(BaseSPSS):
         self.metrics.add_metrics('all',
                                  LF0_RMSE_Hz=metrics.LF0Distortion())
 
+    def normaliser_sources(self):
+        return {
+            'dur': data.MeanVarianceNormaliser('dur'),
+            'lab': data.MinMaxNormaliser('lab'),
+            'counters': data.MinMaxNormaliser('counters'),
+            'lf0': data.MeanVarianceNormaliser('lf0', use_deltas=True),
+        }
+
     def train_data_sources(self):
         return {
-            'n_frames': data_sources.TextSource('n_frames'),
-            'n_phones': data_sources.TextSource('n_phones'),
-            'dur': data_sources.TextSource('dur', normalisation='mvn'),
-            'lab': data_sources.NumpyBinarySource('lab', normalisation='minmax'),
-            'counters': data_sources.NumpyBinarySource('counters', normalisation='minmax'),
-            'lf0': data_sources.NumpyBinarySource('lf0', normalisation='mvn', use_deltas=True),
+            'n_frames': data_sources.TextSource('n_frames', sentence_level=True),
+            'n_phones': data_sources.TextSource('n_phones', sentence_level=True),
+            'dur': data_sources.TextSource('dur'),
+            'lab': data_sources.NumpyBinarySource('lab'),
+            'counters': data_sources.NumpyBinarySource('counters'),
+            'lf0': data_sources.NumpyBinarySource('lf0', use_deltas=True),
             'vuv': data_sources.NumpyBinarySource('vuv'),
         }
 
@@ -70,7 +80,7 @@ class F0Model(BaseSPSS):
         n_frames = features['n_frames']
 
         # Run the encoder.
-        pred_norm_lf0_deltas = self.recurrent_layers(model_inputs, seq_len=n_frames)
+        pred_norm_lf0_deltas = self.layers(model_inputs, seq_len=n_frames)
 
         # Prepare the outputs.
         pred_lf0_deltas = self.normalisers['lf0'].denormalise(pred_norm_lf0_deltas, deltas=True)
@@ -80,24 +90,25 @@ class F0Model(BaseSPSS):
         pred_lf0 = viz.synthesis.MLPG(pred_lf0_deltas, global_variance, padding_size=100, seq_len=n_frames)
 
         outputs = {
-            'lf0_deltas': pred_lf0_deltas,
+            'normalised_lf0_deltas': pred_norm_lf0_deltas,
             'lf0': pred_lf0
         }
 
         return outputs
 
     def loss(self, features, output_features):
-        inputs = features['lf0_deltas']
-        outputs = output_features['lf0_deltas']
         seq_len = features['n_frames']
+
+        loss = losses.mse(output_features['normalised_lf0_deltas'], features['normalised_lf0_deltas'], seq_len)
 
         self.metrics.accumulate(
             self.mode,
-            LF0_RMSE_Hz=(features['lf0'], output_features['lf0'], seq_len, features['vuv']))
+            LF0_RMSE_Hz=(features['lf0'], output_features['lf0'], features['vuv'], seq_len))
 
-        return self._loss(inputs, outputs, seq_len)
+        return loss
 
     def analysis_for_valid_batch(self, features, output_features, out_dir, sample_rate=16000, **kwargs):
+        kwargs['sample_rate'] = sample_rate
         super(F0Model, self).analysis_for_valid_batch(features, output_features, out_dir, **kwargs)
 
         # Synthesise outputs using WORLD.
