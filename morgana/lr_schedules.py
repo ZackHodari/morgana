7fff4f73d08bdac8d2cc7d7861f0d0108c2ebecd
Zack Hodari <zackhodari@gmail.com>
2020-08-14 14:46:52 +0100

    Added CyclicNoamLR learning rate schedule.
    This schedule repeats the NoamLR schedule either,
    - every 'cycle_steps' batches, or
    - when the LR scale value drops below 'cycle_trigger'.

--- morgana/lr_schedules.py
+++ morgana/lr_schedules.py
@@ -90,3 +90,57 @@ class NoamLR(lr_scheduler._LRScheduler):
 SUPPORTED['noam'] = NoamLR
 
 
+class CyclicNoamLR(NoamLR):
+    r"""Cyclical Noam learning rate schedule.
+
+    Same increase and decrease pattern, but it repeats when the scale gets to cycle_trigger (after cycle_steps batches).
+
+              ^                                                        ^
+             / \                                                      / \
+            /   `                                                    /   `
+           /     `                                                  /     `
+          /         `                                              /         `
+         /               `                                        /               `
+        /                       `                                /                       `
+       /                                   `                    /                                   `
+      /                                                    `   /                                                    `
+     /                                                        /
+    /                                                        /
+
+    Parameters
+    ----------
+    optimizer : torch.optim.Optimizer
+        Optimiser instance to modify the learning rate of.
+    warmup_steps : int
+        The number of steps to linearly increase the learning rate.
+    cycle_trigger : float
+        Scale of learning rate that will trigger cycle to repeat.
+    cycle_steps : int, optional
+        The number of steps at which the pattern will repeat. If given, overrides cycle_trigger.
+
+    Notes
+    -----
+    The cycle is triggered based on a given scale during the decreasing stage:
+    cycle_trigger = (warmup_steps ^ 0.5) / (cycle_steps ^ 0.5)
+
+    Becomes,
+    cycle_steps = (cycle_trigger / warmup_steps ^ 0.5) ^ -2
+    """
+    def __init__(self, optimizer, warmup_steps=4000, cycle_trigger=0.2, cycle_steps=None):
+        self.warmup_steps = warmup_steps
+
+        if cycle_steps is None:
+            self.cycle_steps = int((cycle_trigger / self.warmup_steps ** 0.5) ** -2)
+        else:
+            self.cycle_steps = cycle_steps
+
+        super(CyclicNoamLR, self).__init__(optimizer, warmup_steps=warmup_steps)
+
+    def get_lr(self):
+        last_epoch = max(1, self.last_epoch % self.cycle_steps)
+        scale = self.scale(last_epoch)
+        return [base_lr * scale for base_lr in self.base_lrs]
+
+
+SUPPORTED['cyclic_noam'] = CyclicNoamLR
+
